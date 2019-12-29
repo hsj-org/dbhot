85a821f4476cfab8215f7035c64606333f67a115
HsjLoveJava <1251972537@qq.com>
2019-12-29 17:43:11 +0800

    Update master.java

--- master.java
+++ master.java
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-public class Test{
-	public Test{
-=======
-public class TestDemo{
-	public static void main(String[] args) {
-		System.out.println("Hello world")
->>>>>>> master
+public class master {
+	public master {
 	}
 }
